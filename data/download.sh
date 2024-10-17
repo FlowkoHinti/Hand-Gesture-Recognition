feefdd8acaa5037f01c92bf13ccd60927cd2ac42
Patrick Hofmann <phofmann.its-m2023@fh-salzburg.ac.at>
2024-10-17 11:38:13 +0200

    added first handdedection with pretrained model

---
+++ data/download.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#SBATCH --job-name=clean_dicom                            # Job name
+#SBATCH --output=Log/%x_%j.out                                  # Output file (includes job name and ID)
+#SBATCH --error=Log/%x_%j.err                                   # Error file (includes job name and ID)
+#SBATCH --gres=gpu:1                                        # Number of GPUs
+#SBATCH --ntasks=1                                          # Number of processes
+#SBATCH --time=0-04:00                                      # Runtime in D-HH:MM
+#SBATCH --mem-per-cpu=2G                                    # Memory per CPU allocated
+#SBATCH --cpus-per-task=1                                 # CPU cores requested per task
+
+
+wget https://rndml-team-cv.obs.ru-moscow-1.hc.sbercloud.ru/datasets/hagrid_v2/hagridv2_512.zip
+
+
+exit
