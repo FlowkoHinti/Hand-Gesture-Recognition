b11dcb0469f8865cd5887b250251409bc0a5fb2e
Patrick Hofmann <phofmann.its-m2023@fh-salzburg.ac.at>
2024-10-18 22:01:39 +0200

    added something

--- data/unzip.sh
+++ data/unzip.sh
@@ -5,12 +5,12 @@
 #SBATCH --error=Log/%x_%j.err                                   # Error file (includes job name and ID)
 #SBATCH --gres=gpu:1                                        # Number of GPUs
 #SBATCH --ntasks=1                                          # Number of processes
-#SBATCH --time=0-04:00                                      # Runtime in D-HH:MM
-#SBATCH --mem-per-cpu=2G                                    # Memory per CPU allocated
-#SBATCH --cpus-per-task=1                                 # CPU cores requested per task
+#SBATCH --time=1-00:00                                      # Runtime in D-HH:MM
+#SBATCH --mem-per-cpu=4G                                    # Memory per CPU allocated
+#SBATCH --cpus-per-task=2                                 # CPU cores requested per task
 
 
 unzip hagridv2_512.zip
-unzip annotations
+unzip annotations.zip
 
 exit
