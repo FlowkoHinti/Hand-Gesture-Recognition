c7a5f8cab8c572c2b42eb9b7aa925511b5c3a2a3
Patrick Hofmann <phofmann.its-m2023@fh-salzburg.ac.at>
2024-11-13 17:25:08 +0100

    added something

--- 2_Modelling/Transforms/Transforms.py
+++ 2_Modelling/Transforms/Transforms.py
@@ -1,5 +1,6 @@
 import numpy as np
 from torchvision.transforms import v2
+from torchvision.transforms.functional import crop
 import torch
 
 # Custom transform to crop image based on the bounding box
@@ -31,3 +32,23 @@ class CropToBBox:
         # Add Resnet/Vit Transforms
 
         return transformed_target
+    
+# for usage
+class BBoxtoImageNet:
+    def __call__(self, image, x1, y1, x2, y2):
+        # To PIL Image (required for torchvision transforms)
+        cropped_image = v2.ToImage()(image)
+
+        # Crop the image using the bounding box
+        width = x2 - x1
+        height = y2 - y1
+        cropped_image = crop(image, top=y1, left=x1, height=height, width=width)
+
+        # scale the image to 224x224
+        cropped_image = v2.Resize((224, 224))(cropped_image)
+
+        cropped_image = v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)])(cropped_image)
+
+        cropped_image = v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])(cropped_image)
+
+        return cropped_image.unsqueeze(int(0))
